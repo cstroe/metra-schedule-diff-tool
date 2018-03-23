dfd2fbdd84abeb96fdb15fb8dada941945280aaa
Cosmin Stroe <cosmin@mailbox.org>
2018-03-22 21:17:36 -0500

    Add a Time class.

---
+++ src/main/java/Time.java
@@ -0,0 +1,22 @@
+import static java.lang.String.format;
+
+public class Time {
+    public final int hour;
+    public final int minute;
+    public final boolean am;
+
+    public Time(String hour, String minute, String ampm) {
+        this.hour = Integer.parseInt(hour);
+        this.minute = Integer.parseInt(minute);
+        am = "am".equals(ampm.toLowerCase());
+    }
+
+    @Override
+    public String toString() {
+        if(am) {
+            return format("%d:%02d AM", hour, minute);
+        } else {
+            return format("%d:%02d PM", hour, minute);
+        }
+    }
+}
---
+++ src/test/java/TimeTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TimeTest {
+    @Test
+    public void toStringTest() {
+        assertEquals("1:20 AM", new Time("1","20", "AM").toString());
+        assertEquals("11:20 AM", new Time("11","20", "AM").toString());
+        assertEquals("11:02 AM", new Time("11","2", "AM").toString());
+        assertEquals("11:02 PM", new Time("11","2", "PM").toString());
+    }
+}
