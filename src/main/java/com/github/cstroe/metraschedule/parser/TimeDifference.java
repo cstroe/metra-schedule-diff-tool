b42a0c4adb94dae53335d87424779d4867219395
Cosmin Stroe <cosmin@mailbox.org>
2018-03-28 00:56:09 -0500

    Time difference computed correctly across hour boundaries.

--- src/main/java/com/github/cstroe/metraschedule/parser/TimeDifference.java
+++ src/main/java/com/github/cstroe/metraschedule/parser/TimeDifference.java
@@ -2,15 +2,32 @@ package com.github.cstroe.metraschedule.parser;
 
 import com.github.cstroe.metraschedule.domain.Time;
 
+import static java.lang.String.format;
+
 public class TimeDifference {
     public static String diff(Time t1, Time t2) {
         int hourDiff = t2.hour - t1.hour;
         int minDiff = t2.minute - t1.minute;
 
-        if(minDiff > 0) {
-            return "+" + minDiff;
-        } else {
-            return "-" + Math.abs(minDiff);
+        StringBuilder builder = new StringBuilder();
+
+        if(t2.minute < t1.minute && hourDiff > 0) {
+            hourDiff = (t2.hour - 1) - t1.hour;
+            minDiff = (t2.minute + 60) - t1.minute;
         }
+
+        if(t2.minute > t1.minute && hourDiff < 0) {
+            hourDiff = t2.hour - (t1.hour - 1);
+            minDiff = t2.minute - (t1.minute + 60);
+        }
+
+        if(hourDiff != 0) {
+            builder.append(format("%+d", hourDiff));
+            builder.append(":");
+            builder.append(format("%02d", Math.abs(minDiff)));
+        } else {
+            builder.append(format("%+02d", minDiff));
+        }
+        return builder.toString();
     }
 }
--- src/test/java/com/github/cstroe/metraschedule/parser/TimeDifferenceTest.java
+++ src/test/java/com/github/cstroe/metraschedule/parser/TimeDifferenceTest.java
@@ -8,9 +8,21 @@ import static org.junit.Assert.*;
 public class TimeDifferenceTest {
     @Test
     public void compute() {
-        Time t1 = Time.of(1, 30, true);
-        Time t2 = Time.of(1, 45, true);
-        assertEquals("+15", TimeDifference.diff(t1, t2));
-        assertEquals("-15", TimeDifference.diff(t2, t1));
+        {
+            Time t1 = Time.of(1, 30, true);
+            Time t2 = Time.of(1, 45, true);
+            assertEquals("+15", TimeDifference.diff(t1, t2));
+            assertEquals("-15", TimeDifference.diff(t2, t1));
+        }{
+            Time t1 = Time.of(1, 30, true);
+            Time t2 = Time.of(2, 45, true);
+            assertEquals("+1:15", TimeDifference.diff(t1, t2));
+            assertEquals("-1:15", TimeDifference.diff(t2, t1));
+        }{
+            Time t1 = Time.of(1, 30, true);
+            Time t2 = Time.of(2, 29, true);
+            assertEquals("+59", TimeDifference.diff(t1, t2));
+            assertEquals("-59", TimeDifference.diff(t2, t1));
+        }
     }
 }
